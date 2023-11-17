e33006c7eec7e4b39ae66f7485b9f4194f428c66
Tanmay Gurav <tanmayvgurav@gmail.com>
2023-11-17 13:13:37 +0530

    fixed design

--- PCP/assets/js/pcpdefault.js
+++ PCP/assets/js/pcpdefault.js
@@ -13,10 +13,3 @@ function getUser() {
     }
 }
 
-function encodeQuery(data) {
-    let query = data.url
-    for (let d in data.params)
-        query += encodeURIComponent(d) + '='
-            + encodeURIComponent(data.params[d]) + '&';
-    return query.slice(0, -1)
-}
